4f3ff8c5e193fcbe64e9c4b8413b64505cd521bc
Bonnie Zhou <bonniez@google.com>
2018-12-18 15:54:34 -0500

    tablet breakpoint with drawer

--- app.js
+++ app.js
@@ -4,8 +4,28 @@ import {MDCDrawer} from "@material/drawer";
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 
-const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-topAppBar.setScrollTarget(document.getElementById('main-content'));
-topAppBar.listen('MDCTopAppBar:nav', () => {
-  drawer.open = !drawer.open;
-});
+const drawerElement = document.querySelector('.mdc-drawer');
+
+const initDismissibleDrawer = () => {
+  drawerElement.classList.add("mdc-drawer--dismissible");
+  const drawer = MDCDrawer.attachTo(drawerElement);
+  drawer.open = false;
+  topAppBar.setScrollTarget(document.getElementById('main-content'));
+  topAppBar.listen('MDCTopAppBar:nav', () => {
+    drawer.open = !drawer.open;
+  });
+}
+if (window.matchMedia("(max-width: 900px)").matches) {
+  initDismissibleDrawer();
+}
+
+const resizeHandler = () => {
+  if (window.matchMedia("(max-width: 900px)").matches) {
+    initDismissibleDrawer();
+  } else {
+    drawerElement.classList.remove("mdc-drawer--dismissible");
+    drawer.open = true;
+    !!drawer && drawer.destroy();
+  }
+}
+window.addEventListener('resize', resizeHandler);
