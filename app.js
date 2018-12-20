5b7a65d719d0f3740ded6901b5d83bda1232bfdf
Bonnie Zhou <bonniez@google.com>
2018-12-20 15:49:24 -0500

    style according to spec and init list

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 import {MDCTopAppBar} from '@material/top-app-bar/index';
 import {MDCDrawer} from "@material/drawer";
+import {MDCList} from "@material/list";
+
 
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
-
+const listEl = document.querySelector('.mdc-drawer .mdc-list');
 const drawerElement = document.querySelector('.mdc-drawer');
+const mainContentEl = document.querySelector('.main-content');
+
+const topAppBar = new MDCTopAppBar(topAppBarElement);
 
 const initModalDrawer = () => {
   drawerElement.classList.add("mdc-drawer--modal");
   const drawer = MDCDrawer.attachTo(drawerElement);
   drawer.open = false;
-  topAppBar.setScrollTarget(document.getElementById('main-content'));
+  topAppBar.setScrollTarget(mainContentEl);
   topAppBar.listen('MDCTopAppBar:nav', () => {
     drawer.open = !drawer.open;
   });
+
+  listEl.addEventListener('click', (event) => {
+    drawer.open = false;
+  });
 }
+
 if (window.matchMedia("(max-width: 900px)").matches) {
   initModalDrawer();
+} else {
+  const list = new MDCList(listEl);
+  list.wrapFocus = true;
 }
 
 const resizeHandler = () => {
