37d8dabcaeaba2005f293c9c2cdbc41d334ffb5e
Bonnie Zhou <bonniez@google.com>
2019-01-08 14:05:52 -0800

    match Glitch project and update webpack for security

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ import {MDCTopAppBar} from '@material/top-app-bar/index';
 import {MDCDrawer} from "@material/drawer";
 import {MDCList} from "@material/list";
 
+// Select DOM elements
 
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const listEl = document.querySelector('.mdc-drawer .mdc-list');
 const drawerElement = document.querySelector('.mdc-drawer');
 const mainContentEl = document.querySelector('.main-content');
 
+// Initialize top app bar
+
 const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+// Initialize drawer
 
 const initModalDrawer = () => {
   drawerElement.classList.add("mdc-drawer--modal");
@@ -30,6 +35,8 @@ if (window.matchMedia("(max-width: 900px)").matches) {
   const list = new MDCList(listEl);
   list.wrapFocus = true;
 }
+
+// Toggle between permanent drawer and modal drawer at breakpoint 900px
 
 const resizeHandler = () => {
   if (window.matchMedia("(max-width: 900px)").matches) {
